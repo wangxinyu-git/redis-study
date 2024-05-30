5efbf832d21a434e3824cb34937a7e8651ee5592
wangxinyu <shenzhenwangxy@sina.com>
2024-05-31 00:12:59 +0800

    2024-05-30-03

--- hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
+++ hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.hmdp.mapper")
 @SpringBootApplication
 public class HmDianPingApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(HmDianPingApplication.class, args);
     }
--- hm-dianping/src/main/java/com/hmdp/service/IUserService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IUserService.java
@@ -1,11 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.hmdp.dto.LoginFormDTO;
-import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -16,13 +12,5 @@ import javax.servlet.http.HttpSession;
  * @since 2021-12-22
  */
 public interface IUserService extends IService<User> {
-
-    Result sendCode(String phone, HttpSession session);
-
-    Result login(LoginFormDTO loginForm, HttpSession session);
-
-    Result sign();
-
-    Result signCount();
 
 }
